cd39fd7ebed55c6666db07fbb3f43faf876f27cb
yopex <quentin.truffart@gmail.com>
2012-11-27 17:58:03 +0100

    Amazon service

--- MonkeyMusicCloud.Utilities/ImageGoogleSearch.cs
+++ MonkeyMusicCloud.Utilities/ImageGoogleSearch.cs
@@ -1,11 +1,37 @@
-﻿using MonkeyMusicCloud.Utilities.Interface;
+﻿using System;
+using System.Net;
+using MonkeyMusicCloud.Utilities.Amazon;
+using MonkeyMusicCloud.Utilities.Interface;
+using MusicBrainz;
 
 namespace MonkeyMusicCloud.Utilities
 {
     public class ImageGoogleSearch : IImageSearch
     {
-        public string GetImagePath(string search)
+        public string GetImagePath(string album)
         {
+           
+            KeywordRequest req = new KeywordRequest();
+
+            req.keyword = album;
+
+            // Remember to register to get your own devtag.
+
+            req.devtag = "AKIAIKJ5PIY4YWNB6NQQ";
+            req.mode = "music";
+            req.type = "lite";
+            req.page = "1";
+
+            using (AmazonSearchPortClient search = new AmazonSearchPortClient())
+            {
+                ProductInfo productInfo = search.KeywordSearchRequest(req);
+                if (productInfo.Details.Length > 0)
+                {
+                    string url = productInfo.Details[0].ImageUrlLarge;
+                    //webClient.DownloadFile(url, dirName + "/" + albumTitle + ".jpg");
+                }
+
+            }
             return "../../Themes/Images/MusiqueFolder.png";
         }
     }
