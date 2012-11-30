079ae8026178a473f2343118b7cfea9b2f292ab7
yopex <quentin.truffart@gmail.com>
2012-11-30 15:48:24 +0100

    Nettoyage + api google image

--- MonkeyMusicCloud.Utilities/ImageGoogleSearch.cs
+++ MonkeyMusicCloud.Utilities/ImageGoogleSearch.cs
@@ -1,13 +1,18 @@
-﻿using MonkeyMusicCloud.Utilities.Amazon;
+﻿using System.Collections.Generic;
+using Google.API.Search;
 using MonkeyMusicCloud.Utilities.Interface;
 
 namespace MonkeyMusicCloud.Utilities
 {
     public class ImageGoogleSearch : IImageSearch
     {
-        public string GetImagePath(string album)
-        {
-            return "../../Themes/Images/MusiqueFolder.png";
+        public string GetImagePath(string search)
+        {/*
+            GimageSearchClient client = new GimageSearchClient("http://www.google.com");
+            
+            IList<IImageResult> results = client.Search(search, 1);
+            return results[0].Url;*/
+            return search;
         }
     }
 }
